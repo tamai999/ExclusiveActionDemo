5b311143746ae679679aa31bf792626ec9c5d06d
tamai999 <>
2025-06-18 21:46:27 +0900

    アクションを排他するサンプル実装

--- TCAActionLock/Sample/ContentView.swift
+++ TCAActionLock/Sample/ContentView.swift
@@ -1,24 +1,59 @@
-//
-//  ContentView.swift
-//  TCAActionLock
-//
-//  Created by developer on 2025/06/18.
-//
-
+import ComposableArchitecture
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+@Reducer
+struct ContentFeature {
+  @ObservableState
+  struct State: LockState {
+    var isLocked: Bool = false
+    var counter = 0
+  }
+  
+  enum Action: LockAction {
+    case tapButton
+    case updateState(Int)
+    
+    // ロックするアクション、アンロックするアクションを定義
+    var lockMode: ActionLockMode {
+      switch self {
+      case .tapButton:
+          .lock
+      case .updateState:
+          .unlock
+      }
     }
+  }
+  
+  var body: some ReducerOf<Self> {
+    LockReduce { state, action in
+      switch action {
+      case .tapButton:
+        let next = state.counter + 1
+        return .run { send in
+          // 何か重い処理
+          try? await Task.sleep(for: .seconds(1))
+          // 処理結果反映
+          await send(.updateState(next))
+        }
+        
+      case let .updateState(next):
+        state.counter = next
+        return .none
+      }
+    }
+  }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+  @Bindable public var store: StoreOf<ContentFeature>
+  
+  var body: some View {
+    VStack {
+      Button("Tap me") {
+        store.send(.tapButton)
+      }
+      Text("counter: \(store.counter)")
+    }
+    .padding()
+  }
 }
--- TCAActionLock/Sample/TCAActionLockApp.swift
+++ TCAActionLock/Sample/TCAActionLockApp.swift
@@ -1,17 +1,15 @@
-//
-//  TCAActionLockApp.swift
-//  TCAActionLock
-//
-//  Created by developer on 2025/06/18.
-//
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCAActionLockApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  var body: some Scene {
+    WindowGroup {
+      ContentView(
+        store: Store(initialState: ContentFeature.State()) {
+          ContentFeature()
         }
+      )
     }
+  }
 }
